c3a56282c6d7fdb9ac3c1b66df978b137d83d80c
Aksha rahis <aksharahis68@gmail.com>
2025-06-28 22:42:47 +0530

    Add files via upload

---
+++ course.js
@@ -0,0 +1,25 @@
+
+const express = require('express');
+const router = express.Router();
+const Course = require('../models/Course');
+
+router.post('/', async (req, res) => {
+  try {
+    const course = new Course(req.body);
+    await course.save();
+    res.status(201).json(course);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.get('/', async (req, res) => {
+  try {
+    const courses = await Course.find();
+    res.json(courses);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+module.exports = router;
