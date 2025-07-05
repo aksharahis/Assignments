78d9d63f3cd1b46c181a63b531086d9a98a6cf7d
Aksha rahis <aksharahis68@gmail.com>
2025-07-05 22:20:46 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const cors = require('cors');
-const dotenv = require('dotenv');
-const authRoutes = require('./routes/auth');
-const courseRoutes = require('./routes/course');
-
-dotenv.config();
+const Task = require('./models/Task');
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/courses', courseRoutes);
+mongoose.connect('mongodb://localhost:27017/todo-app', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log('MongoDB connected');
-    app.listen(process.env.PORT, () => console.log('Server started on port ' + process.env.PORT));
-  })
-  .catch(err => console.error(err));
+// Create Task
+app.post('/tasks', async (req, res) => {
+    const task = new Task(req.body);
+    await task.save();
+    res.send(task);
+});
+
+// Get All Tasks
+app.get('/tasks', async (req, res) => {
+    const tasks = await Task.find();
+    res.send(tasks);
+});
+
+// Update Task
+app.put('/tasks/:id', async (req, res) => {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.send(task);
+});
+
+// Delete Task
+app.delete('/tasks/:id', async (req, res) => {
+    await Task.findByIdAndDelete(req.params.id);
+    res.send({ success: true });
+});
+
+app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
