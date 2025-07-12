38903403fe5d97c3cffe86a49e1b1e59e5b843dd
Aksha rahis <aksharahis68@gmail.com>
2025-07-12 12:28:17 +0530

    Add files via upload

---
+++ recipeRoutes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const Recipe = require('../models/Recipe');
+
+router.get('/', async (req, res) => {
+    try {
+        const recipes = await Recipe.find();
+        res.json(recipes);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+router.post('/', async (req, res) => {
+    const recipe = new Recipe({
+        title: req.body.title,
+        ingredients: req.body.ingredients,
+        instructions: req.body.instructions,
+        category: req.body.category
+    });
+    try {
+        const newRecipe = await recipe.save();
+        res.status(201).json(newRecipe);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+module.exports = router;
